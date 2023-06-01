094c0c9e07071f2a377dba28a871d0f3cca246d8
Catlover0224 <dmsdn0224@gmail.com>
2023-06-01 16:49:01 +0900

    방명록 생성

---
+++ src/main/java/com/javaex/controller/GuestBookController.java
@@ -0,0 +1,22 @@
+package com.javaex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javaex.service.GuestBookService;
+
+@Controller
+@RequestMapping("/guestBook")
+public class GuestBookController {
+
+	@Autowired
+	private GuestBookService guestBookService;
+	
+	@RequestMapping("/addListForm")
+	public String addListForm() {
+		System.out.println("GuestBookController.addListForm");
+		
+		return "/guestbook/addList";
+	}
+}
--- src/main/webapp/WEB-INF/views/include/nav.jsp
+++ src/main/webapp/WEB-INF/views/include/nav.jsp
@@ -5,7 +5,7 @@
 
 <div id="nav">
 	<ul>
-		<li><a href="">방명록</a></li>
+		<li><a href="${pageContext.request.contextPath}/guestBook/addListForm">방명록</a></li>
 		<li><a href="">갤러리</a></li>
 		<li><a href="${pageContext.request.contextPath}/board/list">게시판</a></li>
 		<li><a href="">입사지원서</a></li>
